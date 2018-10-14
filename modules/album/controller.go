2f9522aa23205fd75e5b9941b014e873b45e8608
kamrujjaman <kamrujjaman.khan@ekkbaz.com>
2018-10-15 01:10:54 +0600

    change index route

--- modules/album/controller.go
+++ modules/album/controller.go
@@ -18,11 +18,7 @@ type Controller struct {
 
 // Index GET /
 func (c *Controller) Index(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("welcome to the era of golang"))
-	return
+	respond.With(w, r, http.StatusOK, "welcome to the era of golang")
 }
 
 //GET albums
